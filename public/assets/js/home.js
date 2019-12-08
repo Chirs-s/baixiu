6bcb65f3f5951f1b2aea985bc5b7055ee662c3ed
cs <cs@qq.com>
2019-12-08 21:26:55 +0800

    轮播图滚动提交

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,32 +1,35 @@
-function banner() {
-    //
+
+function banner(){
     var swiper = Swipe(document.querySelector('.swipe'), {
         auto: 3000,
         transitionEnd: function (index) {
-            // index++;
-
-            $('.cursor span').eq(index).addClass('active').siblings('.active').removeClass('active');
+          // index++;
+  
+          $('.cursor span').eq(index).addClass('active').siblings('.active').removeClass('active');
         }
-    });
-
-    // 上/下一张
-    $('.swipe .arrow').on('click', function () {
+      });
+  
+      // 上/下一张
+      $('.swipe .arrow').on('click', function () {
         var _this = $(this);
-
+  
         if (_this.is('.prev')) {
-            swiper.prev();
+          swiper.prev();
         } else if (_this.is('.next')) {
-            swiper.next();
+          swiper.next();
         }
-    })
+      })
 }
 
 //展示轮播图
 $.ajax({
-    type:'post',
-    url:'slides',
+    type:'get',
+    url:'/slides',
     success:function(data){
     console.log(data);
-
+    //渲染到页面中
+    var html = template('slidesTpl',{data:data});
+    $('#homeIndex').html(html)
+    banner()
     }
 })
