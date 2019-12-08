8acf0a8a1d95d7fd902f06211e1d47066443d4c5
cs <cs@qq.com>
2019-12-08 10:27:39 +0800

    网站设置提交

---
+++ public/assets/js/settings.js
@@ -0,0 +1,42 @@
+
+
+//上传图像提交功能
+$('#logo').on('change',function(){
+    var formData = new FormData();
+    formData.append('image',this.files[0])
+    $.ajax({
+        type:'post',
+        url:'/upload',
+        data:formData,
+        processData:false,
+        contentType:false,
+        success:function(data){
+            console.log(data);
+           $('#site_logo').val(data[0].image);
+           $('#settingImg').attr('src',data[0].image)
+        }
+    })
+});
+//表单数据提交功能
+$('#settingForm').on('submit',function(){
+    
+    var obj = serializeobj($(this));
+    if(!obj.comment){
+        obj.comment =false;
+    }
+    if(!obj.review){
+        obj.review=false
+    }
+    
+    //发送请求
+    $.ajax({
+        type:'post',
+        url:'/settings',
+        data:obj,
+        success:function(data){
+            console.log(data);
+            location.reload()
+        }
+    });
+    return false;
+})
